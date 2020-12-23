244cacbe1d96abc7b9aa2209f614e5c2f66b83ec
Maxim Klyuev <max@2327.ru>
2020-12-24 00:14:46 +0300

    added hamburgers.js

---
+++ hamburgers.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const size = [
+  {size: 'small', price: 20, cal: 20},
+  {size: 'big', price: 100, cal: 40}
+];
+
+const topping = [
+  {title: 'cheesee', price: 10, cal: 20},
+  {title: 'salad', price: 20, cal: 5},
+  {title: 'potatoes', price: 15, cal: 10}
+];
+
+const stuffing = [
+  {title: 'sauce', price: 15, cal: 0},
+  {title: 'mayo', price: 20, cal: 0}
+]
+
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+  }
+  addTopping(topping) {
+    this.color = 'red';
+  }
+  calculatePrice() {
+    if (typeof this.topping !== 'undefined') {
+      let total = this.size.price + this.stuffing.price + this.topping.price;
+      return total;
+    } else {
+      let total = this.size.price + this.stuffing.price;
+      return total;
+    };
+  }
+/*  removeTopping(topping) { // Убрать добавку }
+  getToppings(topping) {   // Получить список добавок }
+  getSize() {              // Узнать размер гамбургера }
+  getStuffing() {          // Узнать начинку гамбургера }
+  calculatePrice() {       // Узнать цену }
+  calculateCalories() {    // Узнать калорийность }*/
+}
+
+
+const hamburger = new Hamburger(size[0], stuffing[0]);
+console.log(hamburger.calculatePrice());
